b0075e4baf1cf66720666863e0ba0043ab60502f
nkipi999 <neeraj.s.kumar@kipi.ai>
2024-10-04 19:22:22 +0530

    proc in dev

--- 200_VENDORS/002_REFINED/1005_SP/R__200_002_1005_00000001_proc_raw_to_refined_ingestion.sql
+++ 200_VENDORS/002_REFINED/1005_SP/R__200_002_1005_00000001_proc_raw_to_refined_ingestion.sql
@@ -10,12 +10,29 @@ HANDLER = 'main'
 EXECUTE AS CALLER
 AS
 $$
-import snowflake.snowpark as snowpark
-from snowflake.snowpark.types import StringType, StructType, StructField
-from snowflake.snowpark.functions import col, call_udf
-import requests
-import pandas as pd
+import snowflake.snowpark.functions as F
+from snowflake.snowpark.session import Session
 
-def main(session: snowpark.Session):
-  return  'yes';
+def main(session: Session) -> str:
+    # Define the tables
+    raw_first_table = session.table("{{ env }}_VENDORS.RAW.{{ env }}_VENDORS_RAW_FIRST")
+    raw_second_table = session.table("{{ env }}_VENDORS.RAW.{{ env }}_VENDORS_RAW_SECOND")
+    
+
+    # Union both tables
+    unioned_data = raw_first_table.select(F.concat(F.col("FIRST_NAME"), F.lit(" "), F.col("LAST_NAME")).alias("FULL_NAME"), "PHONE") \
+                                  .unionAll(
+                                      raw_second_table.select(F.concat(F.col("FIRST_NAME"), F.lit(" "), F.col("LAST_NAME")).alias("FULL_NAME"), "PHONE")
+                                  )
+
+    ##Split the data based on the condition (more than 2 '9's in the phone number)
+    refined_first_insert = unioned_data.filter(F.length(F.regexp_replace(F.col("PHONE"), "[^9]", "")) <= 2)
+    refined_second_insert = unioned_data.filter(F.length(F.regexp_replace(F.col("PHONE"), "[^9]", "")) > 2)
+    
+
+    ##Insert into REFINED schema tables
+    refined_first_insert.write.save_as_table("{{ env }}_VENDORS.REFINED.{{ env }}_VENDORS_REFINED_FIRST", mode="append")
+    refined_second_insert.write.save_as_table("{{ env }}_VENDORS.REFINED.{{ env }}_VENDORS_REFINED_SECOND", mode="append")
+
+    return "Data inserted successfully into REFINED tables."
 $$;
--- 300_PEOPLESOFT/002_REFINED/1005_SP/R__300_002_1005_00000001_proc_raw_to_refined_ingestion.sql
+++ 300_PEOPLESOFT/002_REFINED/1005_SP/R__300_002_1005_00000001_proc_raw_to_refined_ingestion.sql
@@ -1,5 +1,5 @@
 USE DATABASE {{ env }}_PEOPLESOFT;
-USE SCHEMA REFINED;
+USE SCHEMA  REFINED;
 
 CREATE OR REPLACE {{ env }}_PEOPLESOFT_RAW_TO_REFINED_INGESTION()
 RETURNS VARCHAR
@@ -10,12 +10,29 @@ HANDLER = 'main'
 EXECUTE AS CALLER
 AS
 $$
-import snowflake.snowpark as snowpark
-from snowflake.snowpark.types import StringType, StructType, StructField
-from snowflake.snowpark.functions import col, call_udf
-import requests
-import pandas as pd
+import snowflake.snowpark.functions as F
+from snowflake.snowpark.session import Session
 
-def main(session: snowpark.Session):
-  return  'yes';
+def main(session: Session) -> str:
+    # Define the tables
+    raw_first_table = session.table("{{ env }}_PEOPLESOFT.RAW.{{ env }}_PEOPLESOFT_RAW_FIRST")
+    raw_second_table = session.table("{{ env }}_PEOPLESOFT.RAW.{{ env }}_PEOPLESOFT_RAW_SECOND")
+    
+
+    # Union both tables
+    unioned_data = raw_first_table.select(F.concat(F.col("FIRST_NAME"), F.lit(" "), F.col("LAST_NAME")).alias("FULL_NAME"), "PHONE") \
+                                  .unionAll(
+                                      raw_second_table.select(F.concat(F.col("FIRST_NAME"), F.lit(" "), F.col("LAST_NAME")).alias("FULL_NAME"), "PHONE")
+                                  )
+
+    ##Split the data based on the condition (more than 2 '9's in the phone number)
+    refined_first_insert = unioned_data.filter(F.length(F.regexp_replace(F.col("PHONE"), "[^9]", "")) <= 2)
+    refined_second_insert = unioned_data.filter(F.length(F.regexp_replace(F.col("PHONE"), "[^9]", "")) > 2)
+    
+
+    ##Insert into REFINED schema tables
+    refined_first_insert.write.save_as_table("{{ env }}_PEOPLESOFT.REFINED.{{ env }}_PEOPLESOFT_REFINED_FIRST", mode="append")
+    refined_second_insert.write.save_as_table("{{ env }}_PEOPLESOFT.REFINED.{{ env }}_PEOPLESOFT_REFINED_SECOND", mode="append")
+
+    return "Data inserted successfully into REFINED tables."
 $$;
